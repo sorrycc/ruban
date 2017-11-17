bfbd4435123c3cd9e8fa5b800ea4e84b891c2f96
chencheng <sorrycc@gmail.com>
2017-11-17 15:04:38 +0800

    more configs

--- src/getBabelConfig.js
+++ src/getBabelConfig.js
@@ -3,19 +3,16 @@ module.exports = function () {
   return {
     babelrc: false,
     presets: [
-      [require.resolve('babel-preset-af-react'), process.env.NODE_TARGET ? {
-        targets: {
+      [require.resolve('babel-preset-af-react'), {
+        targets: process.env.NODE_TARGET ? {
           node: 6,
-        }
-      } : {}]
+        } : null,
+        disableTransform: process.env.DISABLE_TRANSFORM_RUNTIME
+      }]
     ],
     plugins: [
       require.resolve('babel-plugin-add-module-exports'),
       require.resolve('@babel/plugin-transform-modules-commonjs')
-    ].concat(
-      process.env.DISABLE_TRANSFORM_RUNTIME ? [] : [
-        require.resolve('@babel/plugin-transform-runtime')
-      ]
-    )
+    ]
   };
 };
